d9bbe10934595b911916d9d0d801ba8d735b248a
kaotickj <warhamr1488@gmail.com>
2021-08-21 03:28:30 -0400

    Update nsscmdshell.php

--- nsscmdshell.php
+++ nsscmdshell.php
@@ -112,7 +112,7 @@
             file_put_contents('nsscmdshell.php', file_get_contents('https://raw.githubusercontent.com/kaotickj/The-Not-So-Simple-PHP-Command-Shell/main/nsscmdshell.php'));
             }
         if (isset($_GET['links'])) {
-           echo '<p>Quick Links:</p>
+           echo '<div style="position:fixed;float:right;top:5%;right:5%;"><p>Quick Links:</p>
         <a href="https://stationx-public-download.s3.us-west-2.amazonaws.com/nmap_cheet_sheet_v7.pdf" target="_blank">Nmap Cheat Sheet</a>&nbsp;&nbsp;<a href="https://www.exploit-db.com/" target="_blank">ExploitDB</a>&nbsp;&nbsp;
         <a href="https://github.com/frizb/MSF-Venom-Cheatsheet" target="_blank">Msfvenom Cheat Sheet</a>&nbsp;&nbsp;
         <a href="https://cxsecurity.com/exploit/" target="_blank" target="_blank">CX Security Vulnerability Database</a>&nbsp;&nbsp;<a href="https://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet" target="_blank">Reverse Shell Cheat Sheet</a><br><a href="https://int0x33.medium.com/day-76-use-nishang-empire-and-other-ps1-scripts-manually-332b4604e2a7" target="_blank">Nishang Usage</a>&nbsp;&nbsp;<a href="https://int0x33.medium.com/day-63-top-10-essential-nmap-scripts-for-web-app-hacking-c7829ff5ab7" target="_blank">Esstl Nmap Scripts</a>&nbsp;&nbsp;<a href="https://int0x33.medium.com/day-26-the-complete-list-of-windows-post-exploitation-commands-no-powershell-999b5433b61e" target="_blank">The Complete List of Windows Post-Exploitation Commands</a><br>';
